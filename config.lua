16f9bf0477fca1cca692560bc1635a668b3325f3
DevconDev <devon.hopkins333@gmail.com>
2022-11-11 09:55:42 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,9 +6,9 @@ Config.Timer = {}
 Config.PoliceJob = { "police", "bcso"}
 
 -- Enable if you only want to send alerts to onDuty officers
-Config.OnDutyOnly = false
+Config.OnDutyOnly = true
 
-Config.PoliceAndAmbulance = { "police", "ambulance", "bcso"}
+Config.PoliceAndAmbulance = { "police", "ambulance", "bcso", "fire"}
 Config.PhoneModel = 'prop_npc_phone_02'
 
 -- sets report chance to 100%
